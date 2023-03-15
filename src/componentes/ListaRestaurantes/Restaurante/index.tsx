82c29096cf5eb4401258fb8c55b4dca7168f1a4e
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-03-15 20:25:56 -0300

    feat: listando restaurantes e pratos da api

--- src/componentes/ListaRestaurantes/Restaurante/index.tsx
+++ src/componentes/ListaRestaurantes/Restaurante/index.tsx
@@ -1,4 +1,7 @@
-import IRestaurante from '../../../interfaces/IRestaurante';
+import axios from 'axios';
+import IPrato from 'interfaces/IPrato';
+import IRestaurante from 'interfaces/IRestaurante';
+import { useEffect, useState } from 'react';
 import Prato from '../Prato';
 import estilos from './Restaurante.module.scss';
 
@@ -8,12 +11,20 @@ interface RestauranteProps {
 
 const Restaurante = ({ restaurante }: RestauranteProps) => {
 
+  const [pratos, setPratos] = useState<IPrato[]>()
+
+  useEffect(() => {
+    axios.get<IPrato[]>(`http://localhost:8000/api/v1/restaurantes/${restaurante.id}/pratos/`)
+      .then(resposta => setPratos(resposta.data))
+      .catch(error => console.error(error))
+  }, [restaurante.id]);
+
   return (<section className={estilos.Restaurante}>
     <div className={estilos.Titulo}>
       <h2>{restaurante.nome}</h2>
     </div>
     <div>
-      {restaurante.pratos?.map(item => <Prato prato={item} key={item.id} />)}
+      {pratos?.map(item => <Prato prato={item} key={item.id} />)}
     </div>
   </section>)
 }
--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,24 +1,37 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import IRestaurante from '../../interfaces/IRestaurante';
+import IRestaurante from 'interfaces/IRestaurante';
+import {IPaginacao} from 'interfaces/IPaginacao';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 
 const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+  const [proximaPagina, setProximaPagina] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:8000/api/v1/restaurantes/')
+    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
       .then(resposta => {
         setRestaurantes(resposta.data.results)
+        setProximaPagina(resposta.data.next)
       })
-      .catch( error => console.log(error))
+      .catch( error => console.error(error))
   }, []);
+
+  const verMais = () => {
+    axios.get<IPaginacao<IRestaurante>>(proximaPagina)
+    .then(resposta => {
+      setRestaurantes([...restaurantes, ...resposta.data.results])
+      setProximaPagina(resposta.data.next)
+    })
+    .catch( error => console.error(error))
+  }
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+    {proximaPagina && <button onClick={verMais}> Ver mais </button>}
   </section>)
 }
 
