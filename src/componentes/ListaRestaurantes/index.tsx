40b89aa6de6e7fa8f71585b74514d877322f9ff3
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-03-16 01:36:24 -0300

    feat: criacao de ordenacao e estilo do filtro

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -16,6 +16,7 @@ const ListaRestaurantes = () => {
   const [proximaPagina, setProximaPagina] = useState('');
   const [paginaAnterior, setPaginaAnterior] = useState('');
   const [busca, setBusca] = useState('');
+  const [ordenacao, setOrdenacao] = useState('');
 
   function carregarDados(url:string, opcoes: AxiosRequestConfig = {}) {
     axios.get<IPaginacao<IRestaurante>>(url, opcoes)
@@ -38,6 +39,10 @@ const ListaRestaurantes = () => {
       opcoes.params.search = busca
     }
 
+    if(ordenacao) {
+      opcoes.params.ordering = ordenacao
+    }
+
     carregarDados('http://localhost:8000/api/v1/restaurantes/', opcoes)
   }
 
@@ -56,9 +61,26 @@ const ListaRestaurantes = () => {
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
-    <form onSubmit={buscar}>
-      <input type="text" value={busca} onChange={evento => setBusca(evento.target.value)} />
-      <button type="submit"> Pesquisar </button>
+    <form className={style.filtro} onSubmit={buscar}>
+      <div className={style.filtro__pesquisa}>
+        <input type="text" value={busca} onChange={evento => setBusca(evento.target.value)} placeholder="Buscar restaurantes" />
+      </div>
+      <div className={style.filtro__pesquisa}>
+        <label htmlFor="select-ordenacao">Ordenação</label>
+        <select
+          name="select-ordenacao"
+          id="select-ordenacao"
+          value={ordenacao}
+          onChange={evento => setOrdenacao(evento.target.value)}
+        >
+          <option value="">Padrão</option>
+          <option value="id">Por ID</option>
+          <option value="nome">Por Nome</option>
+        </select>
+      </div>
+      <div className={style.filtro__pesquisa}>
+        <button type='submit'>Buscar</button>
+      </div>
     </form>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
     {<button onClick={() => carregarDados(paginaAnterior)} disabled={!paginaAnterior}> Página Anterior </button>}
