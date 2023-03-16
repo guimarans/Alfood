fdc580faafcafec372f11cfd4257651932d8d1af
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-03-16 00:10:55 -0300

    feat: implementacao de atualizar e deletar restaurante

--- src/paginas/Administracao/Restaurante/AdministracaoRestaurante.tsx
+++ src/paginas/Administracao/Restaurante/AdministracaoRestaurante.tsx
@@ -1,7 +1,8 @@
 import IRestaurante from "interfaces/IRestaurante";
 import { useEffect, useState } from "react";
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button } from '@mui/material';
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function AdministracaoRestaurantes() {
 
@@ -13,18 +14,42 @@ export default function AdministracaoRestaurantes() {
       .catch(erro => console.error(erro))
   }, [])
 
+  function excluir(restauranteAhSerExcluido: IRestaurante) {
+    axios.delete(`http://localhost:8000/api/v2/restaurantes/${restauranteAhSerExcluido.id}/`)
+      .then(() => {
+        const listaRestaurante = restaurantes.filter(restaurante => restaurante.id !== restauranteAhSerExcluido.id)
+        setRestaurantes([...listaRestaurante]);
+      })
+  }
+
   return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
           <TableRow>
             <TableCell> Nome </TableCell>
+            <TableCell> Editar </TableCell>
+            <TableCell> Excluir </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {restaurantes.map(restaurante =>
             <TableRow key={restaurante.id}>
-              <TableCell> {restaurante.nome} </TableCell>
+              <TableCell>
+                {restaurante.nome}
+              </TableCell>
+              <TableCell>
+                [ <Link to={`/admin/restaurantes/${restaurante.id}`}> Editar </Link>]
+              </TableCell>
+              <TableCell>
+                <Button
+                  variant="outlined"
+                  color="error"
+                  onClick={() => excluir(restaurante)}
+                >
+                  Excluir
+                </Button>
+              </TableCell>
             </TableRow>
           )}
         </TableBody>
--- src/paginas/Administracao/Restaurante/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurante/FormularioRestaurante.tsx
@@ -1,22 +1,44 @@
 import { Alert, Button, TextField } from '@mui/material';
 import axios from 'axios';
-import { useState } from 'react';
+import IRestaurante from 'interfaces/IRestaurante';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 
 export default function FormularioRestaurante() {
 
+  const parametros = useParams();
   const [nomeRestaurante, setNomeRestaurante] = useState('');
+
+  useEffect(() => {
+    if (parametros.id) {
+      axios.get<IRestaurante>(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`)
+        .then(resposta => setNomeRestaurante(resposta.data.nome))
+    }
+  }, [parametros]);
 
   function aoSubmeterForm(evento: React.FormEvent<HTMLFormElement>) {
     evento.preventDefault();
 
-    axios.post('http://localhost:8000/api/v2/restaurantes/', { nome: nomeRestaurante })
-      .then(() => {
-        return (
-          <Alert severity="success" color="info">
-            Cadastro realizado com sucesso
-          </Alert>
-        )
+
+    if(parametros.id) {
+      axios.put<IRestaurante>(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`, {
+        nome: nomeRestaurante 
       })
+      .then(() => (
+        <Alert severity="success" color="info">
+          Cadastro atualizado com sucesso
+        </Alert>
+      ))
+    } else {
+      axios.post('http://localhost:8000/api/v2/restaurantes/', { 
+        nome: nomeRestaurante 
+      })
+      .then(() => (
+        <Alert severity="success" color="info">
+          Cadastro realizado com sucesso
+        </Alert>
+      ))
+    }
   }
 
   return (
