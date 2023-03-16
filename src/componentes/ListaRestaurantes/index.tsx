c810cebdafa466e9c42f25303d98bbadc0c21ed3
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-03-16 00:27:30 -0300

    feat:requisicoes com put e delete busca de formulario de restaurante com requisicao get enviando parametro

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,24 +1,44 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
 import IRestaurante from 'interfaces/IRestaurante';
 import {IPaginacao} from 'interfaces/IPaginacao';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 
+interface IParametrosBusca {
+  ordering?: string
+  search?: string
+}
+
 const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
   const [proximaPagina, setProximaPagina] = useState('');
   const [paginaAnterior, setPaginaAnterior] = useState('');
+  const [busca, setBusca] = useState('');
 
-  function carregarDados(url:string) {
-    axios.get<IPaginacao<IRestaurante>>(url)
+  function carregarDados(url:string, opcoes: AxiosRequestConfig = {}) {
+    axios.get<IPaginacao<IRestaurante>>(url, opcoes)
       .then(resposta => {
         setRestaurantes(resposta.data.results)
         setProximaPagina(resposta.data.next)
         setPaginaAnterior(resposta.data.previous)
       })
       .catch( error => console.error(error))
+  }
+
+  function buscar(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+
+    const opcoes = {
+      params: {} as IParametrosBusca
+    }
+
+    if(busca) {
+      opcoes.params.search = busca
+    }
+
+    carregarDados('http://localhost:8000/api/v1/restaurantes/', opcoes)
   }
 
   useEffect(() => {
@@ -36,6 +56,10 @@ const ListaRestaurantes = () => {
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    <form onSubmit={buscar}>
+      <input type="text" value={busca} onChange={evento => setBusca(evento.target.value)} />
+      <button type="submit"> Pesquisar </button>
+    </form>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
     {<button onClick={() => carregarDados(paginaAnterior)} disabled={!paginaAnterior}> Página Anterior </button>}
     {<button onClick={() => carregarDados(proximaPagina)} disabled={!proximaPagina}> Proxima pagina </button>}
